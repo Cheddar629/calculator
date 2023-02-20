fb24f67631cd901188817f9ec99021fe574fbcc7
Cheddar629 <franciscoyan629@gmail.com>
2023-02-20 10:36:02 -0300

    Add basic operation functions

---
+++ script.js
@@ -0,0 +1,24 @@
+let currentNum1;
+let currentNum2;
+let currentOperator;
+let result;
+
+// function that adds two numbers
+function add (a, b) {
+    return a + b
+}
+
+// function that subtracts two numbers
+function subtract (a, b) {
+    return a - b
+}
+
+// function that divides two numbers
+function multiply (a, b) {
+    return a * b
+}
+
+// function that divides two numbers 
+function divide (a, b) {
+    return a / b
+}
