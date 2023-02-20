a6cd40efe912aa22a94eb8793a0c28b6aa1edde3
Cheddar629 <franciscoyan629@gmail.com>
2023-02-20 10:39:25 -0300

    Create function that evaluates the operations

--- script.js
+++ script.js
@@ -5,20 +5,30 @@ let result;
 
 // function that adds two numbers
 function add (a, b) {
-    return a + b
+    return a + b;
 }
 
 // function that subtracts two numbers
 function subtract (a, b) {
-    return a - b
+    return a - b;
 }
 
-// function that divides two numbers
+// function that multiplies two numbers
 function multiply (a, b) {
-    return a * b
+    return a * b;
 }
 
 // function that divides two numbers 
 function divide (a, b) {
-    return a / b
+    return a / b;
+}
+
+// function that will do the operations
+function operate (a, b, operator) {
+    if (operator === 'add') result = add(a, b);
+    if (operator === 'subtract') result = subtract(a, b);
+    if (operator === 'multiply') result = multiply(a, b);
+    if (operator === 'divide') result = divide(a, b);
+
+    return result;
 }
